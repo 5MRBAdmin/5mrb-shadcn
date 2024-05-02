b8d27d7f2ee3020552dba74c096a929571a9087b
5MRBAdmin <5mrb.msoc@gmail.com>
2024-05-01 22:38:08 -0500

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<title>5MRB • Home</title>
+
+<div class="container mx-auto">
+	<h1 class="flex justify-center">Welcome to 5MRB</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,5 @@
+<title>5MRB • About</title>
+
+The 5th Marine Raiders Battalion is an elite MilSim (military simulation) gaming group primarily
+focused on the game of
+<a class="font-bold underline" href="https://www.joinsquad.com/" target="”_blank”">Squad</a>.
